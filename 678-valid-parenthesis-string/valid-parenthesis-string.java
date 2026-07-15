9cafde2a7ca4d798a6ca51b4cad50a501f2360d6
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2026-07-15 23:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.77%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '*') {
+                open++;
+            } else { 
+                open--;
+            }
+            if (open < 0) {
+                return false; 
+            }
+        }
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if (ch == ')' || ch == '*') {
+                close++;
+            } else { 
+                close--;
+            }
+            if (close < 0) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+}
