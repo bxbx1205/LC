43726b09e906b591be1d7bb2fe86c13b457bc5e6
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2026-07-18 15:43:12 +0530

    Time: 3 ms (87.52%) | Memory: 47.8 MB (16.61%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,37 +1,25 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n=ratings.length;
-        int candy=0;
+        int n = ratings.length;
+        int[] candy = new int[n];
+        Arrays.fill(candy, 1);
 
-        int[] left=new int[n];
-        int[] right=new int[n];
-
-        left[0]=1;
-        for(int i =1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
-            }
-            else{
-                left[i]=1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
             }
         }
-        right[n-1]=1;
 
-        for(int i =n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
-            }
-            else{
-                right[i]=1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
             }
         }
-        System.out.println(Arrays.toString(left));
-        System.out.println(Arrays.toString(right));
+        int total = 0;
 
-        for(int i=0;i<n;i++){
-            ratings[i]=Math.max(left[i],right[i]);
-            candy+=ratings[i];
+        for (int num : candy) {
+            total += num;
         }
-        return candy;
+        return total;
     }
 }
